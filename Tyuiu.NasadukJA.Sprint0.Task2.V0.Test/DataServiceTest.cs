5dba9c7b5e11703cc1af5cc4f58e074b304da206
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-09-10 23:29:55 +0500

    Task2

---
+++ Tyuiu.NasadukJA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NasadukJA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.NasadukJA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {   
+            var name = "Юля";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Юля", res);
+        }
+    }
+}
